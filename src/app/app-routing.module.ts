37a83197b6b180d3d57bb8c084327ee102b67075
shahiel12J <122212517+shahiel12J@users.noreply.github.com>
2023-04-24 14:20:33 +0200

    add-emplyee,nabar,footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"AddEmp" , component: AddEmployeeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
