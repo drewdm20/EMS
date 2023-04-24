60fa23a66a44e653d8f02cd5120a8f13455a148e
ntokozo9 <ntokozoblessingradebe98@gmail.com>
2023-04-24 15:38:10 +0200

    commit view-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { DetailsCardComponent } from './components/details-card/details-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DetailsCardComponent
   ],
   imports: [
     BrowserModule,
