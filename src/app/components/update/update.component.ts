cd818a9c1c4834eb6088d6da3978157b0867d023
shahiel12J <122212517+shahiel12J@users.noreply.github.com>
2023-04-24 16:02:56 +0200

    update pop-up

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-update',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent {
+
+  update(){
+    Swal.fire(
+      'Success',
+      'Employee Successfully Updated',
+      'success'
+    )
+  }
 
 }
