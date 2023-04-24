1836eb2f9d7d56d34964175c9a27420a09d8e7cf
shahiel12J <122212517+shahiel12J@users.noreply.github.com>
2023-04-24 16:00:30 +0200

    details and update routing and editing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { UpdateComponent } from './components/update/update.component';
 import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DetailsCardComponent } from './components/details-card/details-card.component';
 
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:"AddEmp" , component: AddEmployeeComponent},
-  {path: "update", component:UpdateComponent }
+  {path: "update", component:UpdateComponent },
+  {path: "details", component: DetailsCardComponent }
+
   ]
 
 @NgModule({
