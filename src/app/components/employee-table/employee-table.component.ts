06ddb45421d51a1897a41dcc3f5261d451613182
shahiel12J <122212517+shahiel12J@users.noreply.github.com>
2023-04-24 15:32:02 +0200

    delete confirmation

--- src/app/components/employee-table/employee-table.component.ts
+++ src/app/components/employee-table/employee-table.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-employee-table',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-table.component.css']
 })
 export class EmployeeTableComponent {
+
+  delete(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
+  }
 
 }
