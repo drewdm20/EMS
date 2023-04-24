1f48ddd89c891190f2269c6173c63bab6ab64191
Jacqui23 <jacquelynedward98@gmail.com>
2023-04-24 14:23:33 +0200

    delete-popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { DeletePopupComponent } from './components/delete-popup/delete-popup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DeletePopupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/delete-popup/delete-popup.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-delete-popup',
+  templateUrl: './delete-popup.component.html',
+  styleUrls: ['./delete-popup.component.css']
+})
+export class DeletePopupComponent {
+
+}
+
+//Pop-alert when trying to delete an employee from employee management system //
+
+Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#67d3a6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  }
+})
